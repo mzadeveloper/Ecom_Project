cb26230af4b72579209a7452add6373ddf705d9e
Muhammad Ahmad <mahmad@hawklogix.com>
2025-06-16 17:40:34 +0500

    Signup and Login Page

--- Client/src/components/Routes.jsx
+++ Client/src/components/Routes.jsx
@@ -3,6 +3,7 @@ import Home from "../pages/Home";
 import Product from "../pages/Products";
 import Profile from "../pages/Profile";
 import Login from "../pages/Login";
+import SignUp from "../pages/SignUp";
 import AddProduct from "../pages/AddProduct";
 
 export default function App() {
@@ -12,6 +13,8 @@ export default function App() {
       <Route path="/products" element={<Product />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/SignUp" element={<SignUp />} /> 
+       
       <Route path="/add-product" element={<AddProduct />} />
     </Routes>
   );
--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -1,7 +1,49 @@
+import React, { useState } from "react";
+import "./../styles/Login.css";
+
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Login clicked:", { email, password });
+  };
+
   return (
-    <>
-      <h2>Login page </h2>
-    </>
+    <div className="login-wrapper">
+      <div className="login-box">
+        <h2 className="login-title">Welcome Back</h2>
+        <form className="login-form" onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label>Email/UserName</label>
+            <input
+              type="email"
+              placeholder="you@example.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              placeholder="••••••••"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+
+          <button type="submit" className="login-button">Sign In</button>
+        </form>
+        <p className="login-footer">
+          Don’t have an account? <a href="/SignUp">Sign up</a>
+        </p>
+      </div>
+    </div>
   );
 }
